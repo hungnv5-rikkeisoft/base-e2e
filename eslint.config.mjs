22e461ee76771c16eca385e89063e56e0396abf6
Hung Nguyen <hungnv5@rikkeisoft.com>
2024-09-20 13:36:52 +0700

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,16 +5,6 @@ import pluginCypress from "eslint-plugin-cypress/flat";
 export default [
   {
     languageOptions: { globals: globals.browser },
-    rules: {
-      "cypress/no-assigning-return-values": "error",
-      "cypress/no-unnecessary-waiting": "error",
-      "cypress/assertion-before-screenshot": "warn",
-      // "cypress/no-force": "warn",
-      // "cypress/no-async-tests": "error",
-      // "cypress/no-async-before": "error",
-      // "cypress/no-pause": "error",
-      // "cypress/no-debug": "error",
-    },
 
     plugins: {
       cypress: pluginCypress,
@@ -22,4 +12,20 @@ export default [
   },
   pluginJs.configs.recommended,
   pluginCypress.configs.recommended,
+  {
+    rules: {
+      "cypress/no-assigning-return-values": "error",
+      "cypress/assertion-before-screenshot": "warn",
+      // "cypress/no-force": "warn",
+      // "cypress/no-async-tests": "error",
+      // "cypress/no-async-before": "error",
+      // "cypress/no-pause": "error",
+      // "cypress/no-debug": "error",
+      "cypress/unsafe-to-chain-command": "off",
+      "cypress/no-unnecessary-waiting": "off",
+    },
+  },
+  {
+    ignores: ["cypress.config.js"],
+  },
 ];
